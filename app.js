924f78cb72dfc790e896cdc70b7856fb1ae5fef8
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-25 14:59:58 +0100

    ✅ update env

--- app.js
+++ app.js
@@ -4,31 +4,34 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
-const port =  9090;
+
 require("dotenv").config();
 
 //DB connection
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}).then(() => {
-  console.log("Database connected");  
-});
- mongoose.connection.on("error", (err) => {
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  });
+mongoose.connection.on("error", (err) => {
   console.log(err);
 });
+
 //use parsing middlware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
-const userRoutes =require("./routes/user")
+const userRoutes = require("./routes/user");
 //use routes
-app.use("/api",userRoutes);
+app.use("/api", userRoutes);
 
 //start server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
